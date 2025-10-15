2706effa9120f9679f2c20e97f7f48e1997f7a33
dpwl0974 <dpwl0974@gmail.com>
2025-10-15 12:17:11 +0900

    init: 공통 설정 초기 세팅

--- backend/src/main/java/com/backend/petplace/global/exception/BusinessException.java
+++ backend/src/main/java/com/backend/petplace/global/exception/BusinessException.java
@@ -1,4 +1,15 @@
 package com.backend.petplace.global.exception;
 
-public class BusinessException {
+import com.backend.petplace.global.response.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
 }

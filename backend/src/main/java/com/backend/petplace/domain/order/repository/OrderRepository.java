782d920f282fd6abe3154843a675e6d349ef06b3
choijihyuk <jihyuk0609@naver.com>
2025-10-15 12:51:31 +0900

    init : 백엔드 domain 엔티티 이름과 sql 문법 겹치는 문제 해결 @Table(name = "orders"), @Table(name = "users") 사용

--- backend/src/main/java/com/backend/petplace/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/backend/petplace/domain/order/repository/OrderRepository.java
@@ -3,6 +3,6 @@ package com.backend.petplace.domain.order.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.backend.petplace.domain.order.entity.Order;
 
-public interface OrderRepository  extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }

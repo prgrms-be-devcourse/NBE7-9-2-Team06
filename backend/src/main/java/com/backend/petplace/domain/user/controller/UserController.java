f9a5a827f8b01b4e19b37258c6989bec730595e4
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-10-16 09:39:32 +0900

    feat: 회원가입 컨트롤러 추가

--- backend/src/main/java/com/backend/petplace/domain/user/controller/UserController.java
+++ backend/src/main/java/com/backend/petplace/domain/user/controller/UserController.java
@@ -1,12 +1,32 @@
 package com.backend.petplace.domain.user.controller;
 
+import com.backend.petplace.domain.user.entity.User;
+import com.backend.petplace.domain.user.service.UserService;
+import com.backend.petplace.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
-
 @RestController
-@RequestMapping("api/v1/ ")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
+
+  private final UserService userService;
+
+  @PostMapping("/signup")
+  public ResponseEntity<ApiResponse<UserSignupResponse>> signup(
+      @RequestBody UserSignupRequest request) {
+
+    UserSignupResponse response = userService.signup(request);
+    return ResponseEntity.ok(new ApiResponse.success(response));
+  }
+
+  @PostMapping("/login")
+  public void login(@RequestBody User user) {
+    // TODO: user 로그인
+  }
 }

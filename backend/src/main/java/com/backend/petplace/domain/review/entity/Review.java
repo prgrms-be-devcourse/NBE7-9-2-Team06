69910bff4727605be0fd0941efbe79ac3c3ff498
dpwl0974 <dpwl0974@gmail.com>
2025-10-16 09:00:30 +0900

    style: 불필요한 내용 삭제 및 스타일 맞춤

--- backend/src/main/java/com/backend/petplace/domain/review/entity/Review.java
+++ backend/src/main/java/com/backend/petplace/domain/review/entity/Review.java
@@ -3,8 +3,17 @@ package com.backend.petplace.domain.review.entity;
 import com.backend.petplace.domain.place.entity.Place;
 import com.backend.petplace.domain.user.entity.User;
 import com.backend.petplace.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter

169809f3938a6da49c5195d5c1cbe6977ed0be62
Hodakrer <jihyuk0609@naver.com>
2025-10-17 17:20:18 +0900

    feat: 주문 등록과 주문상품 등록 개발 (#32)

--- backend/src/main/java/com/backend/petplace/domain/order/entity/Order.java
+++ backend/src/main/java/com/backend/petplace/domain/order/entity/Order.java
@@ -1,10 +1,20 @@
 package com.backend.petplace.domain.order.entity;
 
+import com.backend.petplace.domain.user.entity.User;
+import com.backend.petplace.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,9 +24,36 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Table(name = "orders")
-public class Order {
+public class Order extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private Long orderId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @NotNull
+  @Min(0)
+  private Long totalPrice;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private OrderStatus orderStatus;
+
+  @Builder
+  public Order(User user, Long totalPrice, OrderStatus orderStatus) {
+    this.user = user;
+    this.totalPrice = totalPrice;
+    this.orderStatus = orderStatus;
+  }
+
+  public static Order createOrder(User user, Long totalPrice) {
+    return Order.builder()
+        .user(user)
+        .totalPrice(totalPrice)
+        .orderStatus(OrderStatus.ORDERED)
+        .build();
+  }
 }
--- backend/src/main/java/com/backend/petplace/domain/orderproduct/entity/OrderProduct.java
+++ backend/src/main/java/com/backend/petplace/domain/orderproduct/entity/OrderProduct.java
@@ -1,9 +1,16 @@
 package com.backend.petplace.domain.orderproduct.entity;
 
+import com.backend.petplace.domain.order.entity.Order;
+import com.backend.petplace.domain.product.entity.Product;
+import com.backend.petplace.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,9 +19,34 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class OrderProduct {
+public class OrderProduct extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private Long orderProductId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "orders", nullable = false)
+  Order order;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product")
+  Product product;
+
+  private long quantity;
+
+  @Builder
+  private OrderProduct(Order order, Product product, long quantity) {
+    this.order = order;
+    this.product = product;
+    this.quantity = quantity;
+  }
+
+  public static OrderProduct createOrderProduct(Order order, Product product, long quantity) {
+    return OrderProduct.builder()
+        .order(order)
+        .product(product)
+        .quantity(quantity)
+        .build();
+  }
 }

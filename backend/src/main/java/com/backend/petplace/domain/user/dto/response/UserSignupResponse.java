58343fef67cb832a7916df031ebae5dcbe0985df
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-10-17 09:45:18 +0900

    fix:Response DTO에 @Getter 추가, /api/vi/** 시큐리티 허용 추가

--- backend/src/main/java/com/backend/petplace/domain/user/dto/response/UserSignupResponse.java
+++ backend/src/main/java/com/backend/petplace/domain/user/dto/response/UserSignupResponse.java
@@ -1,7 +1,9 @@
 package com.backend.petplace.domain.user.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class UserSignupResponse {
 
--- backend/src/main/java/com/backend/petplace/global/config/security/SecurityConfig.java
+++ backend/src/main/java/com/backend/petplace/global/config/security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/h2-console/**").permitAll()
             .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+            .requestMatchers("/api/v1/**").permitAll()
             .anyRequest().authenticated()
         )
         .httpBasic(httpBasic -> httpBasic.disable());

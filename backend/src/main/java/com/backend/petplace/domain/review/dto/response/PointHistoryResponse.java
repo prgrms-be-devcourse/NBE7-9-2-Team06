2e1f028d5e457ae9f861ae3f4fe4635c012ab67d
dpwl0974 <dpwl0974@gmail.com>
2025-10-20 15:12:20 +0900

    feat: 응답 dto 생성자 추가

--- backend/src/main/java/com/backend/petplace/domain/review/dto/response/PointHistoryResponse.java
+++ backend/src/main/java/com/backend/petplace/domain/review/dto/response/PointHistoryResponse.java
@@ -4,8 +4,10 @@ import com.backend.petplace.domain.review.dto.PointTransaction;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @Schema(description = "내 포인트 내역 응답 DTO")
 public class PointHistoryResponse {
 
@@ -15,4 +17,8 @@ public class PointHistoryResponse {
   @Schema(description = "포인트 적립/사용 내역 목록")
   private List<PointTransaction> history;
 
+  public PointHistoryResponse(int totalPoints, List<PointTransaction> history) {
+    this.totalPoints = totalPoints;
+    this.history = history;
+  }
 }

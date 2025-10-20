a34d35dd3c996ec9e71851279278187eb80a69cc
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-10-21 04:21:42 +0900

    chore: 요청된 토큰 확인을 위한 로그 추가

--- backend/src/main/java/com/backend/petplace/global/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/backend/petplace/global/jwt/JwtAuthenticationFilter.java
@@ -56,6 +56,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   private String resolveToken(HttpServletRequest request) {
     String bearerToken = request.getHeader("Authorization");
     if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+      logger.info("Bearer Token: " + bearerToken);
       return bearerToken.substring(7);
     }
     return null;

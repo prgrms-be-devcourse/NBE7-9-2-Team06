2ee94becb83d0db15a601cc918036648f6405ba8
choijihyuk <jihyuk0609@naver.com>
2025-10-20 14:23:52 +0900

    feat : 1인 유저의 주문내역 모두 조회 - 1. Dtok, 컨트롤러, 서비스, 레포지토리 코드 개발 - 2. Product 엔티티에 누락된 'productName' 추가 - 3. 2에 맞춰 기본 상품 생성 시 productName 추가

--- backend/src/main/java/com/backend/petplace/domain/order/service/OrderService.java
+++ backend/src/main/java/com/backend/petplace/domain/order/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.backend.petplace.domain.order.service;
 
 import com.backend.petplace.domain.order.dto.request.OrderCreateRequest;
+import com.backend.petplace.domain.order.dto.response.OrderReadByIdResponse;
 import com.backend.petplace.domain.order.entity.Order;
 import com.backend.petplace.domain.order.repository.OrderRepository;
 import com.backend.petplace.domain.orderproduct.dto.request.OrderProductCreateRequest;
@@ -82,5 +83,13 @@ public class OrderService {
     if (orderProductRequest.getQuantity() > product.getStock()) {
       throw new BusinessException(ErrorCode.NOT_ENOUGH_STOCK);
     }
+  }
+
+  public List<OrderReadByIdResponse> getOrdersByUserId(Long userId) {
+    List<Order> orders = orderRepository.findByUserId(userId);
+
+    return orders.stream()
+        .map(OrderReadByIdResponse::new) // Order -> DTO 변환
+        .toList();
   }
 }
--- backend/src/main/java/com/backend/petplace/global/initdata/BaseInitData.java
+++ backend/src/main/java/com/backend/petplace/global/initdata/BaseInitData.java
@@ -13,27 +13,31 @@ public class BaseInitData implements CommandLineRunner {
   private final ProductRepository productRepository;
 
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) {
 
     Product p1 = Product.builder()
+        .productName("상품 1")
         .price(150L)
         .stock(9999L)
         .description("상품 1")
         .build();
 
     Product p2 = Product.builder()
+        .productName("상품 2")
         .price(80L)
         .stock(9999L)
         .description("상품 2")
         .build();
 
     Product p3 = Product.builder()
+        .productName("상품 3")
         .price(120L)
         .stock(9999L)
         .description("상품 3")
         .build();
 
     Product p4 = Product.builder()
+        .productName("상품 4")
         .price(200L)
         .stock(9999L)
         .description("상품 4")

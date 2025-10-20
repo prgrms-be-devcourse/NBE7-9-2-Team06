7ea8bf1a0412914ea6a42b32dc2913763456d5ca
dpwl0974 <dpwl0974@gmail.com>
2025-10-20 14:17:54 +0900

    feat: 조회 메서드 추가

--- backend/src/main/java/com/backend/petplace/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/backend/petplace/domain/review/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.backend.petplace.domain.review.repository;
 
+import com.backend.petplace.domain.place.entity.Place;
 import com.backend.petplace.domain.review.entity.Review;
 import com.backend.petplace.domain.user.entity.User;
 import java.util.List;
@@ -8,5 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
   List<Review> findByUserOrderByIdDesc(User user);
+
+  List<Review> findByPlaceOrderByIdDesc(Place place);
 
 }

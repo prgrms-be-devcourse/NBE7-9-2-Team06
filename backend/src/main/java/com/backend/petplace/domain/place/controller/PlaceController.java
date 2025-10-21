adddab64403e929c375330bee213d52ae3e1bced
Ho-JunYu <hojoon1011@gmail.com>
2025-10-21 11:23:33 +0900

    refactor: PlaceController에 PlaceSpecification 구현 및 @PathVariable 추가

--- backend/src/main/java/com/backend/petplace/domain/place/controller/PlaceController.java
+++ backend/src/main/java/com/backend/petplace/domain/place/controller/PlaceController.java
@@ -5,19 +5,20 @@ import com.backend.petplace.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/places")
 @RequiredArgsConstructor
-public class PlaceController {
+public class PlaceController implements PlaceSpecification{
 
   private final PlaceService placeService;
 
   @GetMapping("/{id}")
-  public ResponseEntity<ApiResponse<PlaceDetailResponse>> getPlaceDetail(Long id) {
+  public ResponseEntity<ApiResponse<PlaceDetailResponse>> getPlaceDetail(@PathVariable Long id) {
 
-    return ResponseEntity.ok(ApiResponse.success(placeService.getPlaceDetail));
+    return ResponseEntity.ok(ApiResponse.success(placeService.getPlaceDetail(id)));
   }
 }

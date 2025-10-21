3c1c88d18cc648186b37ad417b37e213fdfc1b7d
dpwl0974 <dpwl0974@gmail.com>
2025-10-21 11:49:13 +0900

    refactor: 리뷰 스웨거 명세 변경

--- backend/src/main/java/com/backend/petplace/domain/review/controller/ReviewSpecification.java
+++ backend/src/main/java/com/backend/petplace/domain/review/controller/ReviewSpecification.java
@@ -3,7 +3,9 @@ package com.backend.petplace.domain.review.controller;
 import com.backend.petplace.domain.review.dto.request.ReviewCreateRequest;
 import com.backend.petplace.domain.review.dto.response.MyReviewResponse;
 import com.backend.petplace.domain.review.dto.response.PlaceReviewsResponse;
+import com.backend.petplace.domain.review.dto.response.PresignedUrlResponse;
 import com.backend.petplace.domain.review.dto.response.ReviewCreateResponse;
+import com.backend.petplace.domain.review.service.S3Service;
 import com.backend.petplace.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -16,10 +18,14 @@ import org.springframework.web.multipart.MultipartFile;
 @Tag(name = "Review", description = "리뷰 API")
 public interface ReviewSpecification {
 
-  @Operation(summary = "리뷰 등록", description = "특정 장소에 대한 리뷰를 등록합니다. 내용, 별점은 필수이며 이미지는 선택입니다.")
+  @Operation(summary = "리뷰 이미지 Presigned URL 생성", description = "S3에 직접 이미지를 업로드할 수 있는 Presigned URL을 생성합니다.")
+  ResponseEntity<ApiResponse<PresignedUrlResponse>> getPresignedUrl(
+      @Parameter(description = "업로드할 원본 파일명", required = true) String filename
+  );
+
+  @Operation(summary = "리뷰 등록", description = "특정 장소에 대한 리뷰를 등록합니다. 내용, 별점은 필수이며 이미지는 선택입니다. ✅ Presigned URL로 이미지 업로드 후, 리뷰 정보를 최종 저장합니다.")
   ResponseEntity<ApiResponse<ReviewCreateResponse>> createReview(
-      @Parameter(description = "리뷰 정보 - 장소ID, 내용, 별점", required = true) ReviewCreateRequest request,
-      @Parameter(description = "업로드할 이미지 파일 (선택)") MultipartFile image
+      @Parameter(description = "리뷰 정보 - 장소ID, 내용, 별점, S3 이미지 경로", required = true) ReviewCreateRequest request
   );
 
   @Operation(summary = "장소 리뷰 목록 조회", description = "특정 장소에 등록된 모든 리뷰 목록과 별점 평균, 리뷰 총 개수를 조회합니다.")

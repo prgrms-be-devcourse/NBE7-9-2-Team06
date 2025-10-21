154a461b3354ebde653d1a69d1f218f7c18ddd51
Ho-JunYu <hojoon1011@gmail.com>
2025-10-21 11:02:51 +0900

    feat: 장소의 상세 정보를 조회하는 컨트롤러 구현

--- backend/src/main/java/com/backend/petplace/domain/place/controller/PlaceController.java
+++ backend/src/main/java/com/backend/petplace/domain/place/controller/PlaceController.java
@@ -1,12 +1,23 @@
 package com.backend.petplace.domain.place.controller;
 
+import com.backend.petplace.domain.place.service.PlaceService;
+import com.backend.petplace.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/ ")
+@RequestMapping("api/v1/places")
 @RequiredArgsConstructor
 public class PlaceController {
 
+  private final PlaceService placeService;
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ApiResponse<PlaceDetailResponse>> getPlaceDetail(Long id) {
+
+    return ResponseEntity.ok(ApiResponse.success(placeService.getPlaceDetail));
+  }
 }

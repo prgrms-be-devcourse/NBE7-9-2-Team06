3189f86ebec462cf02eda60cf6d29123f59f39a5
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-10-22 07:15:32 +0900

    feat: email 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // email (회원가입 인증)
+    implementation ("org.springframework.boot:spring-boot-starter-mail")
 }
 
 tasks.withType<Test> {

f059670d6f4faa75b76a595fb1f5c87c627c4335
Ho-JunYu <hojoon1011@gmail.com>
2025-10-22 02:02:52 +0900

    feat: PlaceController에 @Validated 추가

--- backend/src/main/java/com/backend/petplace/domain/place/controller/PlaceController.java
+++ backend/src/main/java/com/backend/petplace/domain/place/controller/PlaceController.java
@@ -10,12 +10,14 @@ import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("api/v1/places")
 @RequiredArgsConstructor

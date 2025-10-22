fd17b7de0379753d8eb7b5c1d7bc862427c93628
choijihyuk <jihyuk0609@naver.com>
2025-10-22 11:09:18 +0900

    refactor : 안전하게 userId 받아오기 - authentication.getPrincipal()로 받아왔습니다. - 위에 따라 parameter가 바뀌어서 swagger interface도 고쳤습니다. - rest api 주소도 @RequestMapping("api/v1") 에서 @RequestMapping("api/v1/orders")로 고쳤습니다.

--- backend/src/main/java/com/backend/petplace/domain/order/controller/OrderController.java
+++ backend/src/main/java/com/backend/petplace/domain/order/controller/OrderController.java
@@ -3,11 +3,15 @@ package com.backend.petplace.domain.order.controller;
 import com.backend.petplace.domain.order.dto.request.OrderCreateRequest;
 import com.backend.petplace.domain.order.dto.response.OrderReadByIdResponse;
 import com.backend.petplace.domain.order.service.OrderService;
+import com.backend.petplace.global.exception.BusinessException;
+import com.backend.petplace.global.jwt.CustomUserDetails;
 import com.backend.petplace.global.response.ApiResponse;
+import com.backend.petplace.global.response.ErrorCode;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,38 +21,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController implements OrderSpecification {
 
   private final OrderService orderService;
 
   @Override
-  @PostMapping(value = "/orders")
+  @PostMapping()
   public ResponseEntity<ApiResponse<Void>> createOrder(
-      @RequestBody OrderCreateRequest request,
-      @CookieValue("userId") Long userId) {
+      @RequestBody OrderCreateRequest request) {
 
-    orderService.createOrder(request, userId);
+    orderService.createOrder(request, getUserIdFromJWTToken());
     return ResponseEntity.ok(ApiResponse.success());
   }
 
   @Override
-  @GetMapping(value = "/orders")
-  public ResponseEntity<ApiResponse<List<OrderReadByIdResponse>>> getOrderById(
-      @CookieValue("userId") Long userId) {
+  @GetMapping()
+  public ResponseEntity<ApiResponse<List<OrderReadByIdResponse>>> getOrderById() {
 
-    List<OrderReadByIdResponse> responses = orderService.getOrdersByUserId(userId);
+    List<OrderReadByIdResponse> responses = orderService.getOrdersByUserId(getUserIdFromJWTToken());
     return ResponseEntity.ok(ApiResponse.success(responses));
   }
 
   @Override
-  @PatchMapping("/orders/{orderid}/cancel")
-  public ResponseEntity<ApiResponse<Void>> cancelOrder(
-      @PathVariable("orderid") Long orderId,
-      @CookieValue("userId") Long userId) {
+  @PatchMapping("/{orderid}/cancel")
+  public ResponseEntity<ApiResponse<Void>> cancelOrder(@PathVariable("orderid") Long orderId) {
 
-    orderService.cancelOrder(userId, orderId);
+    orderService.cancelOrder(getUserIdFromJWTToken(), orderId);
     return ResponseEntity.ok(ApiResponse.success());
+  }
+
+  private Long getUserIdFromJWTToken() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null ||
+        !(authentication.getPrincipal() instanceof CustomUserDetails)) {
+      throw new BusinessException(ErrorCode.NOT_FOUND_MEMBER);
+    }
+    CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+    return userDetails.getUserId();
   }
 }

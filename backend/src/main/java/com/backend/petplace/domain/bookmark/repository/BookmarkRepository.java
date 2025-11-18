f68e7f4487d5a817e6580c67c1dad061739172fd
Ho-JunYu <hojoon1011@gmail.com>
2025-11-18 18:08:05 +0900

    feat:BookmarkRepository에 userId와 Place의 Id로 등록된 Bookmark가 있는지 확인하는 메서드 추가

--- backend/src/main/java/com/backend/petplace/domain/bookmark/repository/BookmarkRepository.java
+++ backend/src/main/java/com/backend/petplace/domain/bookmark/repository/BookmarkRepository.java
@@ -1,10 +1,13 @@
 package com.backend.petplace.domain.bookmark.repository;
 
 import com.backend.petplace.domain.bookmark.entity.Bookmark;
+import java.lang.ScopedValue;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
   boolean existsByUserIdAndPlace_Id(Long userId, Long placeId);
 
+  Optional<Bookmark> findByUserIdAndPlace_Id(Long userId, Long placeId);
 }

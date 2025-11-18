6a4a9204791e4eb0b80b94978b5551104279f6e3
Ho-JunYu <hojoon1011@gmail.com>
2025-11-18 22:36:35 +0900

    feat: BookmarkRepository에 findAllByUserIdWithPlace() 메서드 추가

--- backend/src/main/java/com/backend/petplace/domain/bookmark/repository/BookmarkRepository.java
+++ backend/src/main/java/com/backend/petplace/domain/bookmark/repository/BookmarkRepository.java
@@ -1,12 +1,23 @@
 package com.backend.petplace.domain.bookmark.repository;
 
 import com.backend.petplace.domain.bookmark.entity.Bookmark;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
   boolean existsByUserIdAndPlace_Id(Long userId, Long placeId);
 
   Optional<Bookmark> findByUserIdAndPlace_Id(Long userId, Long placeId);
+
+  @Query("""
+      select b 
+      from Bookmark b 
+      join fetch b.place 
+      where b.userId = :userId
+      """)
+  List<Bookmark> findAllByUserIdWithPlace(@Param("userId") Long userId);
 }

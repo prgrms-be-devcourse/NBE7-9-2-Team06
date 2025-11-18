260de60e851b3114af752f066aa4139fc18b433d
Ho-JunYu <hojoon1011@gmail.com>
2025-11-18 21:41:25 +0900

    feat: 북마크 조회 기능 컨트롤러 레이어 구현

--- backend/src/main/java/com/backend/petplace/domain/bookmark/controller/BookmarkController.java
+++ backend/src/main/java/com/backend/petplace/domain/bookmark/controller/BookmarkController.java
@@ -1,14 +1,17 @@
 package com.backend.petplace.domain.bookmark.controller;
 
+import com.backend.petplace.domain.bookmark.dto.response.MyBookmarkPlaceResponse;
 import com.backend.petplace.domain.bookmark.service.BookmarkService;
 import com.backend.petplace.global.jwt.CustomUserDetails;
 import com.backend.petplace.global.response.ApiResponse;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Positive.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +43,15 @@ public class BookmarkController implements BookmarkSpecification{
   ) {
     bookmarkService.removeBookmark(userDetails.getUserId(), placeId);
     return ResponseEntity.ok(ApiResponse.success());
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<ApiResponse<List<MyBookmarkPlaceResponse>>> getMyBookmarks(
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ) {
+    List<MyBookmarkPlaceResponse> results = bookmarkService.getMyBookmarks(userDetails.getUserId());
+
+    return ResponseEntity.ok(ApiResponse.success(results));
   }
 
 }

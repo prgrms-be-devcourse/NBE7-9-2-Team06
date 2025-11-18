9462847e51551264b0cd6038028e631b7ea76d1d
Ho-JunYu <hojoon1011@gmail.com>
2025-11-18 16:19:44 +0900

    refactor: 북마크 추가 경로 수정 및 장소 id PathVariable로 변경

--- backend/src/main/java/com/backend/petplace/domain/bookmark/controller/BookmarkController.java
+++ backend/src/main/java/com/backend/petplace/domain/bookmark/controller/BookmarkController.java
@@ -3,14 +3,14 @@ package com.backend.petplace.domain.bookmark.controller;
 import com.backend.petplace.domain.bookmark.service.BookmarkService;
 import com.backend.petplace.global.jwt.CustomUserDetails;
 import com.backend.petplace.global.response.ApiResponse;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
@@ -21,10 +21,10 @@ public class BookmarkController {
 
   private final BookmarkService bookmarkService;
 
-  @PostMapping
-  public ResponseEntity<ApiResponse<Long>> addBookmark (
+  @PostMapping("/places/{placeId}")
+  public ResponseEntity<ApiResponse<Long>> addBookmark(
       @AuthenticationPrincipal CustomUserDetails userDetails,
-      @RequestParam @NotNull Long placeId
+      @PathVariable @Positive Long placeId
   ) {
     Long userId = userDetails.getUserId();
     Long bookmarkId = bookmarkService.addBookmark(userId, placeId);

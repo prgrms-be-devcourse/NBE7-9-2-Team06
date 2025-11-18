06e7c4f9f767edc1e4ddb7263274c89e78346d58
Ho-JunYu <hojoon1011@gmail.com>
2025-11-18 18:08:48 +0900

    chore: 불필요한 import 정리 및 제거

--- backend/src/main/java/com/backend/petplace/domain/bookmark/repository/BookmarkRepository.java
+++ backend/src/main/java/com/backend/petplace/domain/bookmark/repository/BookmarkRepository.java
@@ -1,7 +1,6 @@
 package com.backend.petplace.domain.bookmark.repository;
 
 import com.backend.petplace.domain.bookmark.entity.Bookmark;
-import java.lang.ScopedValue;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 

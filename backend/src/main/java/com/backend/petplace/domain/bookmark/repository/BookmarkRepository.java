5d92f9d3801bd4be57471d3b9ecdcbd074f3fbde
Ho-JunYu <hojoon1011@gmail.com>
2025-11-18 17:23:40 +0900

    feat: BookmarkRepository에 existsByUserIdAndPlace_Id 메서드 추가

--- backend/src/main/java/com/backend/petplace/domain/bookmark/repository/BookmarkRepository.java
+++ backend/src/main/java/com/backend/petplace/domain/bookmark/repository/BookmarkRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
+  boolean existsByUserIdAndPlace_Id(Long userId, Long placeId);
+
 }
